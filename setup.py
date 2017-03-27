c41de340258ec74c6e9cc7eb3440d53cb3fe0a03
HashSplat <jtengel08@gmail.com>
2017-03-27 11:55:38 -0400

    Minor changes to project files.

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setup(
     description="Python 3 iridium communication library for the iridium 9602 modem.",
     url="https://github.com/HashSplat/pyiridium9602",
 
-    author="Justin Engel",
+    author="SeaLandAire Technologies Inc.",
     author_email="jengel@sealandaire.com",
     
     license="MIT",
