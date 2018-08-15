89f2f26d204dc8eec11373535fa40cb8115d5163
justengel <jtengel08@gmail.com>
2018-08-15 14:32:43 -0400

    updated the version.

--- setup.py
+++ setup.py
@@ -32,10 +32,10 @@ if __name__ == "__main__":
 
     setup(
         name="pyiridium9602",
-        version="1.2.1",
+        version="1.2.2",
         description="Python 3 iridium communication library for the iridium 9602 modem.",
         url="https://github.com/justengel/pyiridium9602",
-        download_url="https://github.com/justengel/pyiridium9602/archive/v1.2.1.tar.gz",
+        download_url="https://github.com/justengel/pyiridium9602/archive/v1.2.2.tar.gz",
 
         keywords=["iridium", "9602", "iridium9602"],
 
